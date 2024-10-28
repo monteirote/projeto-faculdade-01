d444cdb244555a309f0ac097feb897cd93c91f87
Vinicius Monteiro <vinicius@iconax.com.br>
2024-10-28 15:05:32 -0300

    feat: adicionando endpoint post para timeslots

---
+++ Controllers/TimeSlotController.cs
@@ -0,0 +1,38 @@
+﻿using Projeto01.Data;
+using Projeto01.Models;
+using Projeto01.ViewModels;
+
+namespace Projeto01.Controllers {
+
+    [ApiController]
+    [Route("api/timeslot")]
+    public class TimeSlotController : ControllerBase {
+
+        [HttpPost]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> CreateTimeSlot ([FromServices] AppDbContext context, [FromBody] CreateTimeSlotViewModel timeSlot) {
+            try {
+
+                var doctor = await context.Doctors.FirstOrDefault(x => x.Id == timeSlot.doctorId);
+
+                if (doctor is null)
+                    return BadRequest("O médico informado não existe.");
+
+                var newTimeSlot = new TimeSlot {
+                    Doctor = doctor,
+                    StartTime = timeSlot.StartTime,
+                    EndTime = timeSlot.EndTime,
+                    IsAvaliable = true
+                };
+
+                await context.TimeSlots.AddAsync(newTimeSlot);
+                await context.SaveChangesAsync();
+
+                return Ok(newTimeSlot);
+
+            } catch (Exception e) {
+                return StatusCode(500, new { message = "Falha interna no servidor" });
+            }
+        }
+    }
+}
---
+++ ViewModels/TimeSlotViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Projeto01.ViewModels {
+    public class CreateTimeSlotViewModel {
+
+        [Required]
+        public int DoctorId { get; set; }
+
+        [Required]
+        public DateTime StartTime { get; set; }
+
+        [Required]
+        public DateTime EndTime { get; set; }
+    }
+}
