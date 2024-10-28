9d4921ede25b1d8499a207bc198d33070ebe9a57
Vinicius Monteiro <vinicius@iconax.com.br>
2024-10-28 13:52:34 -0300

    feat: Adicionando o Doctor Controller

--- Controllers/DoctorController.cs
+++ Controllers/DoctorController.cs
@@ -4,7 +4,6 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Projeto01.Data;
 using Projeto01.Models;
-using Projeto01.Services;
 using Projeto01.ViewModels;
 
 namespace Projeto01.Controllers {
@@ -13,8 +12,51 @@ namespace Projeto01.Controllers {
     [Route("api/doctor")]
     public class DoctorController : ControllerBase {
 
-        public async IActionResult CreateDoctor ([FromBody] CreateDoctorViewModel doctor, [FromServices] AppDbContext context) {
+        [HttpGet]
+        [Authorize(Roles = "Admin", "Paciente")]
+        public async Task<IActionResult> GetAllDoctorsAsync ([FromServices] AppDbContext context) {
             try {
+                var doctors = await context.Doctors.ToListAsync();
+                return Ok(doctors);
+
+            } catch (Exception e) {
+                return StatusCode(500, new { erro = "Falha interna no servidor", exception = e.Message });
+            }
+        }
+
+
+        [HttpGet("{id}")]
+        [Authorize (Roles = "Admin", "Paciente")]
+        public async Task<IActionResult> GetDoctorById ([FromServices] AppDbContext context, [FromRoute] int id) {
+            try {
+                var doctorFound = await context.Doctors.FindAsync(id);
+
+                if (doctorFound is null)
+                    return NotFound();
+
+                return Ok(doctorFound);
+
+            } catch (Exception e) {
+                return StatusCode(500, new { erro = "Falha interna no servidor", exception = e.Message });
+            }
+        }
+
+
+        [HttpPost]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> CreateDoctor ([FromBody] CreateDoctorViewModel doctor, [FromServices] AppDbContext context) {
+            try {
+
+                var newDoctor = new Doctor {
+                    Name = doctor.Name,
+                    Specialty = doctor.Specialty,
+                    ProfilePicture = doctor.ProfilePicture
+                };
+
+                await context.Doctors.AddAsync(newDoctor);
+                await context.SaveChangesAsync();
+
+                return Ok();
 
             } catch (Exception e) {
                 return StatusCode(500, new { erro = "Falha interna no servidor", exception = e.Message });
