c44e31054d63d9f960ea96086675f957215f2768
Vinicius Monteiro <vinicius@iconax.com.br>
2024-10-30 09:44:06 -0300

    feat: adicionando endpoints para busca e remoção de timeslots

--- Controllers/TimeSlotController.cs
+++ Controllers/TimeSlotController.cs
@@ -56,5 +56,48 @@ namespace Projeto01.Controllers {
                 return StatusCode(500, new { erro = "Falha interna no servidor", exception = e.Message });
             }
         }
+
+
+        [HttpGet("doctor/{id}")]
+        [Authorize(Roles = "Admin, Paciente")]
+        public async Task<IActionResult> GetTimeSlotsByDoctor ([FromServices] AppDbContext context, [FromRoute] int id) {
+            try {
+                var results = await context.TimeSlots
+                                                .Include(x => x.Doctor)
+                                                .Where(x => x.Doctor.Id == id)
+                                                .Where(x => x.StartTime >= DateTime.Now)
+                                                .ToListAsync();
+
+                return Ok(results);
+
+            } catch (Exception e) {
+                return StatusCode(500, new { erro = "Falha interna no servidor", exception = e.Message });
+            }
+        }
+
+
+        [HttpDelete("{id}")]
+        [Authorize (Roles = "Admin, Paciente")]
+        public async Task<IActionResult> CancelTimeSlot ([FromServices] AppDbContext context, [FromRoute] int id) {
+            try {
+                var timeSlotFound = (from ts in context.TimeSlots
+                                      where ts.Id == id
+                                      select ts).FirstOrDefault();
+
+                if (timeSlotFound is null)
+                    return NotFound();
+
+                if (!timeSlotFound.IsAvaliable)
+                    return BadRequest("Não foi possível cancelar esse horário pois já existe uma consulta associada à ele.");
+
+                context.TimeSlots.Remove(timeSlotFound);
+                await context.SaveChangesAsync();
+
+                return NoContent();
+
+            } catch (Exception e) { 
+                return StatusCode(500, new { erro = "Falha interna no servidor", exception = e.Message });
+            }
+        }
     }
 }
